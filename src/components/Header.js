f7730de09e4e7e9e6d38ee271c225f9c1a141de2
Madhur3006 <madhurmangal5@gmail.com>
2023-07-20 21:10:47 +0530

    commit-04

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
 import { Link } from 'react-router-dom';
+import { SEARCH_SUGGESTIONS_URL } from '../utils/CONSTANTS';
 
 const Header = () => {
+
+    const [searchQuery, setSearchQuery] = useState('')
+    const [suggestions, setSuggestions] = useState([])
+    const [showSuggestion, setShowSuggestion] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => getSearchSuggestions(), 200)
+
+        return () => {
+            clearTimeout(timer) 
+        }
+    }, [searchQuery])
+
+    const getSearchSuggestions = async () => {
+        const data = await fetch(SEARCH_SUGGESTIONS_URL+searchQuery)
+        const json = await data.json()
+        setSuggestions(json[1]) 
+    }
 
     const dispatch = useDispatch()
 
@@ -18,8 +37,24 @@ const Header = () => {
                 <img className = 'h-10 w-35 mt-0 cursor-pointer' alt='youtube' src = 'https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6-970-80.jpg.webp' />
             </div>
             <div className='col-span-10'>
-                <input className=' border border-solid border-black rounded-l-full w-3/4 p-2' type = 'text' />
+                <div>
+                    <input className=' border border-solid border-black rounded-l-full w-3/4 p-2'
+                    type = 'text'
+                    value = {searchQuery}
+                    onChange = {(e) => setSearchQuery(e.target.value)} 
+                    onFocus = {() => setShowSuggestion(true)}
+                    onBlur = {() => setShowSuggestion(false)}/>
                 <button className='border border-black rounded-r-full p-2'>Search</button>
+                </div>
+                {showSuggestion && <div className = 'fixed bg-white py-2 px-2 w-[20rem] shadow-lg rounded-lg border border-gray'>
+                    <ul>
+                        {suggestions.map((suggestion, index) => (
+                            <li key = {index} className='py-2 px-3 shadow-sm hover:bg-gray-100'>
+                                {suggestion}
+                            </li>
+                        ))}
+                    </ul>
+                </div>}
             </div>
             <div className='mr-0 col-span-1'>
                 <img className = 'h-8 mr-0' alt='user' src = 'https://cdn-icons-png.flaticon.com/512/3177/3177440.png' />
--- src/utils/CONSTANTS.js
+++ src/utils/CONSTANTS.js
@@ -1,2 +1,3 @@
 const GOOGLE_API_KEY = 'AIzaSyCPYGRA2eXaA25UYZIdZbYBi0z5M06Sv50'
 export const API_URL = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='+GOOGLE_API_KEY
+export const SEARCH_SUGGESTIONS_URL = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
